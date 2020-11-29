796d441b7d0c3121dea7d95bf837bf4c1b3bb453
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2020-11-29 16:49:28 +0200

    Add cities navigation on the map

--- Thermometer/index.js
+++ Thermometer/index.js
@@ -1,38 +1,38 @@
 'use strict';
 
-const requestStr = 'http://api.openweathermap.org/data/2.5/weather?q=Kyiv&lang=ru&units=metric&appid=e3f7748f23d84130cd165d531a86ce00';
-    // apiKey = 'e3f7748f23d84130cd165d531a86ce00',
-    // city = 'Kiev';
+// const requestStr = 'http://api.openweathermap.org/data/2.5/weather?q=Kyiv&lang=ru&units=metric&appid=e3f7748f23d84130cd165d531a86ce00';
+//     // apiKey = 'e3f7748f23d84130cd165d531a86ce00',
+//     // city = 'Kiev';
 
-let rez = undefined;
+// let rez = undefined;
 
-const getWeather = (url, callback, reject = console.error) => {
+// const getWeather = (url, callback, reject = console.error) => {
 
-    const request = new XMLHttpRequest;
+//     const request = new XMLHttpRequest;
 
-    request.open('GET', url);
+//     request.open('GET', url);
     
-    request.addEventListener('readystatechange', () => {
+//     request.addEventListener('readystatechange', () => {
     
-        if (request.readyState !== 4){
-            return;
-        }
+//         if (request.readyState !== 4){
+//             return;
+//         }
 
-        if (request.status === 200){
-            callback(request.response);
-        }else{
-            reject(request.status);
-        }
-    });
+//         if (request.status === 200){
+//             callback(request.response);
+//         }else{
+//             reject(request.status);
+//         }
+//     });
 
-    request.send();
-}
+//     request.send();
+// }
 
-getWeather(requestStr, (data) => {
-    rez = JSON.parse(data);
+// getWeather(requestStr, (data) => {
+//     rez = JSON.parse(data);
 
-    console.log(rez);
-});
+//     console.log(rez);
+// });
 
 
 console.log(128 / 40);
@@ -41,3 +41,67 @@ console.log(128 / 40);
 // 0C = -43deg;
 //+max = 42deg;
 //-max = -170deg;
+
+//Map scrolling
+
+const map = document.querySelector('.map');
+
+const citiesCoords = [
+    {
+        city: 'rivne',
+        x: 225,
+        y: 160,
+    },
+
+    {
+        city: 'lviv',
+        x: 225,
+        y: 95,
+    },
+
+    {
+        city: 'kiev',
+        x: 15,
+        y: 150,
+    },
+
+    {
+        city: 'vinnytsia',
+        x: 140,
+        y: 40,
+    },
+
+    {
+        city: 'zaporizhia',
+        x: -220,
+        y: -55,
+    },
+
+    {
+        city: 'kharkiv',
+        x: -225,
+        y: 130,
+    },
+
+    {
+        city: 'odessa',
+        x: 45,
+        y: -160,
+    }
+];
+
+map.addEventListener('click', (event) => {
+    const target = event.target;
+    
+    if (target.classList.contains('city')){
+        const city = target.dataset.city;
+        
+        const equalCityObj = citiesCoords.find(item => item.city === city);
+
+        map.classList.add('increase');
+        map.style.transform = `scale(2) translateX(${equalCityObj.x}px) translateY(${equalCityObj.y}px)`;
+    }
+})
+
+
+
